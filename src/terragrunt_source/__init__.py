745dbaf85858b956c585ee9305b63d3bbc5be0d9
David D. Riddle <ddriddle@illinois.edu>
2019-11-09 00:42:59 -0600

    Upgrade to support new version of Terragrunt

--- src/terragrunt_source/__init__.py
+++ src/terragrunt_source/__init__.py
@@ -6,8 +6,90 @@ import sys
 
 import hcl
 
+# Python 2.x workaround
+try:
+    FileNotFoundError
+except NameError:  # pragma: no cover
+    FileNotFoundError = IOError  # pragma: no cover
 
-def terragrunt_source():  # type: () -> str
+
+class InvalidPathError(Exception):
+    """Exception raised for invalid paths.
+
+    Args:
+        path: Invalid path.
+    """
+    def __init__(self, path):  # type: (str) -> None
+        self.message = "Invalid directory path: %s" % path
+
+    def __str__(self):  # type: () -> str
+        return self.message
+
+
+def terragrunt_source(filename, tfvars=False):  # type: (str, bool) -> str
+    """Main function for implementing terragrunt_source.
+
+    Args:
+        filename: A Terragrunt configuration file.
+
+    Returns:
+        Returns a string usable with TERRAGRUNT_SOURCE.
+    """
+    root = getRoot()
+    source = getSource(filename, tfvars)
+
+    return getPath(source, root)
+
+
+def getSource(filename, tfvars=False):  # type: (str, bool) -> str
+    """Parses ``filename`` for the source line.
+
+    Args:
+        filename: A Terragrunt configuration file.
+
+    Returns:
+        Returns a Terraform source line.
+    """
+    with open(filename, 'r') as fp:
+        terragrunt = hcl.load(fp)
+
+    if tfvars:
+        terragrunt = terragrunt['terragrunt']
+    source = terragrunt['terraform']['source']
+
+    return source
+
+
+def getPath(source, root):  # type: (str, str) -> str
+    """Calculates a terragrunt source value.
+
+    Args:
+        source: A Terraform source line.
+        root: The path to a repository of module(s).
+
+    Returns:
+        Returns a string usable with TERRAGRUNT_SOURCE.
+    """
+    root = root.rstrip('/')
+
+    src = source.split('//')
+    module = '' if len(src) < 2 else src[1].split('?')[0]
+
+    if module:
+        root += '//' + module
+
+    return root
+
+
+def getRoot():  # type: () -> str
+    """Retrieves the user supplied path to Terraform modules.
+
+    Returns:
+        Returns a system path.
+
+    Raises:
+        InvalidPathError: If the user supplied path is invalid
+    """
     root = os.environ['TERRAGRUNT_DEFAULT_MODULES_REPO']
 
     try:  # Python 2.x
@@ -15,27 +97,40 @@ def terragrunt_source():  # type: () -> str
     except AttributeError:  # Python 3.x
         pass
 
-    with open('terraform.tfvars', 'r') as fp:
-        tfvars = hcl.load(fp)
-
-    source = tfvars['terragrunt']['terraform']['source']
-    path = source.split('//')[1].split('?')[0]
-
-    return root + '//' + path
+    return root
 
 
 def error(code, mesg):  # type: (int, str) -> None
+    """Prints an error message and kills the program.
+
+    Args:
+        code: Exit code.
+        mesg: Error message to print.
+    """
     print(str(mesg), file=sys.stderr)
     exit(code)
 
 
 def main():  # type: () -> None
     try:
-        print(terragrunt_source())
-    except IOError as e:
+        try:
+            source = terragrunt_source('terragrunt.hcl')
+        except FileNotFoundError as e:
+            try:
+                source = terragrunt_source('terraform.tfvars', tfvars=True)
+            except FileNotFoundError:
+                raise e
+
+        if not os.path.isdir(source):
+            raise InvalidPathError(source)
+
+        print(source)
+    except FileNotFoundError as e:
         error(2, str(e))
     except KeyError:
         error(3, 'Environment variable TERRAGRUNT_DEFAULT_MODULES_REPO '
                  'is undefined!')
+    except InvalidPathError as e:
+        error(4, str(e))
 
     exit(0)
--- src/tests/test_terragrunt_source.py
+++ src/tests/test_terragrunt_source.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import unittest
 
 from io import StringIO
@@ -10,26 +11,40 @@ try:
 except ImportError:
     from mock import patch  # type: ignore
 
-from terragrunt_source import main
+from terragrunt_source import main, getPath
 
-VALID_TERRAFORM_CONFIG = """
+TFVARS = """
 terragrunt = {
   include {
     path = "${find_in_parent_folders()}"
   }
 
   terraform {
-    source = "git@github.com:cites-illinois/modules.git//lambda?ref=v0.6.2"
+    source = "git@github.com:org/repo.git//lambda?ref=v0.6.2"
   }
+}
+"""
+
+HCL = """
+include {
+  path = "${find_in_parent_folders()}"
+}
+
+terraform {
+  source = "git@github.com:org/repo.git//lambda?ref=v0.6.2"
 }
 """
 
 
 class TerragruntSource(unittest.TestCase):
-    """ Tests various SAML helper functions """
+    """ Full integration tests """
 
     def setUp(self):
-        # Create temp dir
+        # Create temp modules dir
+        self.modules_path = mkdtemp()
+        self.addCleanup(rmtree, self.modules_path)
+
+        # Create temp working dir
         self.tmpdir = mkdtemp()
         self.addCleanup(rmtree, self.tmpdir)
 
@@ -37,40 +52,94 @@ class TerragruntSource(unittest.TestCase):
         self.addCleanup(chdir, getcwd())
         chdir(self.tmpdir)
 
-    def assertTerragruntSource(self, test_output, environment, code, mesg):
-        with patch(test_output, new=StringIO()) as out:
-            with patch.dict('os.environ', environment, clear=True):
-                with self.assertRaises(SystemExit) as cm:
-                    main()
+    def assertTerragruntSourcePrints(self, environment, code, mesg):
+        """Test ``main()`` return status and output to stdout and stderr.
+
+        Args:
+            environment (Dict[str]): Environment variables to pass.
+            code (int): The expected return code.
+            mesg (str): The expected messsage returned.
+
+        """
+        with patch('sys.stdout', new=StringIO()) as stdout:
+            with patch('sys.stderr', new=StringIO()) as stderr:
+                with patch.dict('os.environ', environment, clear=True):
+                    with self.assertRaises(SystemExit) as cm:
+                        main()
 
         exp = cm.exception
-        self.assertEqual(exp.code, code)
+        self.assertEqual(exp.code, code, stderr.getvalue())
 
-        self.assertEqual(out.getvalue().strip(), mesg)
+        out = stderr if code else stdout
+        self.assertEqual(out.getvalue(), mesg)
+
+    def assertCommon(self, hcl=False, mkdir=True, code=0, mesg="%s"):
+        modules_path = self.modules_path
+        module = '//lambda'
+
+        mesg = mesg % (modules_path + module) + '\n'
+
+        env = {'TERRAGRUNT_DEFAULT_MODULES_REPO': modules_path}
+        if mkdir:
+            os.mkdir(modules_path + module)
+
+        if hcl:
+            with open('terragrunt.hcl', 'w') as f:
+                f.write(HCL)
+        else:
+            with open('terraform.tfvars', 'w') as f:
+                f.write(TFVARS)
+
+        self.assertTerragruntSourcePrints(env, code, mesg)
 
     def test_valid_terraform_tfvars_file(self):
         """succeeds when terraform.tfvars is present and valid. """
-        env = {'TERRAGRUNT_DEFAULT_MODULES_REPO': '/usr/src/modules'}
+        self.assertCommon(hcl=False)
 
-        with open('terraform.tfvars', 'w') as f:
-            f.write(VALID_TERRAFORM_CONFIG)
-
-        mesg = '/usr/src/modules//lambda'
-        self.assertTerragruntSource('sys.stdout', env, 0, mesg)
+    def test_valid_terragrunt_hcl_file(self):
+        """succeeds when terragrunt.hcl is present and valid. """
+        self.assertCommon(hcl=True)
 
     def test_missing_terraform_tfvars_file(self):
-        """returns 2 when terraform.tfvars is missing. """
-        env = {'TERRAGRUNT_DEFAULT_MODULES_REPO': '/usr/src/modules'}
+        """returns 2 when terraform.tfvars and terraform.hcl are missing. """
+        env = {'TERRAGRUNT_DEFAULT_MODULES_REPO': self.modules_path}
 
-        mesg = "[Errno 2] No such file or directory: 'terraform.tfvars'"
-        self.assertTerragruntSource('sys.stderr', env, 2, mesg)
+        mesg = "[Errno 2] No such file or directory: 'terragrunt.hcl'\n"
+        self.assertTerragruntSourcePrints(env, 2, mesg)
 
     def test_missing_environment_var(self):
         """returns 3 when TERRAGRUNT_DEFAULT_MODULES_REPO is unset. """
         mesg = "Environment variable TERRAGRUNT_DEFAULT_MODULES_REPO " \
-               "is undefined!"
+               "is undefined!\n"
 
-        self.assertTerragruntSource('sys.stderr', {}, 3, mesg)
+        self.assertTerragruntSourcePrints({}, 3, mesg)
+
+    def test_bad_paths(self):
+        """returns 1 when TERRAGRUNT_DEFAULT_MODULES_REPO is unset. """
+        mesg = "Invalid directory path: %s"
+
+        self.assertCommon(hcl=True, mkdir=False, code=4, mesg=mesg)
+
+
+class SourceLineTests(unittest.TestCase):
+
+    def assertCommon(self, add_end_slash_to_root=False):
+        source = "git@github.com:org/repo.git?ref=v0.6.2"
+        root = '/usr/src/modules'
+
+        if add_end_slash_to_root:
+            root += '/'
+
+        self.assertEqual(
+            getPath(source, root),
+            '/usr/src/modules',
+        )
+
+    def test_no_double_slash(self):
+        self.assertCommon()
+
+    def test_root_ending_slash(self):
+        self.assertCommon(add_end_slash_to_root=True)
 
 
 if __name__ == '__main__':
